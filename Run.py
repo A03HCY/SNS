8114cf559beb53d4f99d053c696f604f98bc9220
A03HCY <3206921401@qq.com>
2021-02-20 21:57:52 +0800

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,19 @@
+from Core import *
+
+__verson__ = '0.1.2'
+
+class Runner(Network):
+    t_id = None
+
+    def __init__(self, port, token):
+        super().__init__(port, token)
+        pass
+
+    def Getcmd(self):
+        print('Terminal # ')
+        return
+
+    def run(self):
+        pass
+
+a = Network(1010, 'F6987ij42')
