81f640246932454e822dc28a70cd4c2ea8575243
A03HCY <3206921401@qq.com>
2021-02-24 12:34:15 +0800

    Update Run.py

--- Run.py
+++ Run.py
@@ -6,6 +6,7 @@ __verson__ = '0.1.2'
 class Runner(Network):
     Contin = True
     t_id = None
+    t_pt = None
 
     def __init__(self, port, token):
         super().__init__(port, token)
@@ -23,6 +24,18 @@ class Runner(Network):
             for final in part.split(':'):
                 cmd.append(final)
         return cmd
+    
+    def simplifyPath(self, path):
+        path.replace('//', '/')
+        path.replace('\\', '/')
+        list = path.split('/')
+        res =[]
+        for i in range(len(list)):
+            if list[i] == '..' and len(res) > 0:
+                res = res[:-1]
+            elif list[i] != '' and list[i] != '.' and list[i] != '..':
+                res.append(list[i])
+        return '/'+'/'.join(res)
 
     def run(self):
         while self.Contin:
@@ -36,7 +49,9 @@ class Runner(Network):
                 if 'Error' in rs:
                     print(rs)
                     continue
+                self.t_pt = input('Folder > ')
                 self.t_id = rs
+                continue
             if cmd[0] in ['ter']:
                 ter = self.GetTerminal()
                 for key in ter:
@@ -47,7 +62,74 @@ class Runner(Network):
                     print('')
                     for value in ter[key]:
                         print('    ' + value)
+                continue
+            if cmd[0] in ['ls']:
+                rz = self.File(self.t_id, 'list', '', self.t_pt)
+                if 'Error' in rz:
+                    print(rz)
+                    continue
+                for part in rz:
+                    if rz[part] == []:
+                        continue
+                    print(part+':')
+                    for files in rz[part]:print('  '+files)
+                continue
+
+            if cmd[0] in ['cd']:
+                rz = self.File(self.t_id, 'list', '', self.t_pt)
+                if 'Error' in rz:
+                    self.t_pt = input('Reset Path > ')
+                    continue
+                if rz['folders'] == []:
+                    print('No folder can change.')
+                    stay = input('Up folder? > ')
+                    if stay in ['yes', 'y']:
+                        self.t_pt = os.path.join(self.t_pt, '/..')
+                        self.t_pt = self.simplifyPath(self.t_pt)[1:]
+                        print(self.t_pt)
+                    continue
+                print('folders:')
+                i = 1
+                for part in rz['folders']:
+                    print(' '+str(i)+' '+part)
+                    i += 1
+                cmd = int(input('Number > '))
+                if cmd == 0:
+                    self.t_pt = os.path.join(self.t_pt, '/..')
+                    self.t_pt = self.simplifyPath(self.t_pt)[1:]
+                    continue
+                if rz['folders'][cmd-1]:
+                    self.t_pt = os.path.join(self.t_pt, rz['folders'][cmd-1])
+                    self.t_pt = self.simplifyPath(self.t_pt)[1:]
+                print(self.t_pt)
+                continue
+            if cmd[0] in ['get']:
+                rz = self.File(self.t_id, 'list', '', self.t_pt)
+                if 'Error' in rz:
+                    self.t_pt = input('Reset Path > ')
+                    continue
+                if rz['files'] == [] and rz['unknows'] == []:
+                    print('No Files.')
+                    continue
+                fun = rz['files'] + rz['unknows']
+                print('Files and unknows:')
+                i = 1
+                for part in fun:
+                    print(' '+str(i)+' '+part)
+                    i += 1
+                cmd = int(input('Number > '))
+                if fun[cmd-1]:
+                    download = os.path.join(self.t_pt, fun[cmd-1]).replace('\\', '/')
+                    download = download.replace('/', '\\')
+                    print(download)
+                    rz = self.File(self.t_id, 'get', './', download)
+                    print(rz)
+
+
+                
                     
 
 a = Runner(1010, 'F6987ij42')
+a.t_id = a.Connect(('192.168.1.8', 1010), 'F6987ij42')
+a.t_pt = 'x:/'
 a.run()
